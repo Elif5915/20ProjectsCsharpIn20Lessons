60d4203ad4daebf9df8774d60fdbd043cbd762ed
Elif Yıldırım <elifyildirim3@outlook.com>
2025-03-26 23:32:31 +0300

    TriggerOrderStock_11 proje oluşturma,Db First ile geliştirme yapıldı.

---
+++ TriggerOrderStock_11/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriggerOrderStock_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string number;
+            Console.WriteLine("### Sipariş Stok Sistemi");
+            Console.WriteLine();
+            Console.WriteLine("1-Ürün Listesi");
+            Console.WriteLine("2-Sipariş Listesi");
+            Console.WriteLine("3-Yeni Ürün Satışı");
+            Console.WriteLine("4-Ürün Stok Güncelleme");
+            Console.WriteLine();
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine();
+
+            Console.Write("Lütfen yapmak istediğiniz işlemi seçin: ");
+            number = Console.ReadLine();
+
+            if(number == "1")
+            {
+
+            }
+
+        }
+    }
+}
