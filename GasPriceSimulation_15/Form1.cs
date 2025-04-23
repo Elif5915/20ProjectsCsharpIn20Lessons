3edb5d392dbff747c3066c690bf06055a80b342f
Elif Yıldırım <elifyildirim3@outlook.com>
2025-04-23 22:59:05 +0300

    15 proje bitti

--- GasPriceSimulation_15/Form1.cs
+++ GasPriceSimulation_15/Form1.cs
@@ -20,30 +20,81 @@ namespace GasPriceSimulation_15
         double dieselPrice = 42.25;
         double gasolinePrice = 43.22;
         double lpg = 23.73;
-        double gasAmount;
-        double progressBarValue;
-        double totalPrice;
+        double gasAmount = 0;
+        double dieselAmount = 0;
+        double lpgAmount = 0;
+        double progressBarValue = 0;
+        double totalPrice = 0;
+        int count = 0;
         private void btnStart_Click(object sender, EventArgs e)
         {
+            gasAmount = Convert.ToDouble(txtbxGasAmount.Text);
+            dieselAmount = Convert.ToDouble(txtbxGasAmount.Text);
+            lpgAmount = Convert.ToDouble(txtbxGasAmount.Text);
             timer1.Start();
-            timer1.Interval = 500;
+            timer1.Interval = 100;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            gasAmount = Convert.ToDouble(txtbxGasAmount.Text);
-            progressBar1.Value += 5;
             if (rdbBenzin.Checked)
-            { 
-                progressBar1.Value += 3;
-                if(progressBar1.Value == 99)
+            {
+                count++;
+                if (count <= gasAmount)
+                {
+                    totalPrice += gasolinePrice;
+                    txtbxTotalAmount.Text = totalPrice.ToString() + " ₺";
+                }
+                else
+                {
+                    txtbxTotalAmount.Text = totalPrice.ToString() + " ₺";
+                }
+                progressBar1.Value += 1;
+                if (progressBar1.Value == 99)
+                {
+                    timer1.Stop();
+                }
+
+            }
+            if (rdbMotorin.Checked)
+            {
+                count++;
+                if (count <= dieselAmount)
+                {
+                    totalPrice += dieselPrice;
+                    txtbxTotalAmount.Text = totalPrice.ToString() + " ₺";
+                }
+                else
+                {
+                    txtbxTotalAmount.Text = totalPrice.ToString() + " ₺";
+                }
+                progressBar1.Value += 1;
+                if (progressBar1.Value == 99)
+                {
+                    timer1.Stop();
+                }
+
+            }
+            if (rdbLpg.Checked)
+            {
+                count++;
+                if (count <= lpgAmount)
+                {
+                    totalPrice += lpg;
+                    txtbxTotalAmount.Text = totalPrice.ToString() + " ₺";
+                }
+                else
+                {
+                    txtbxTotalAmount.Text = totalPrice.ToString() + " ₺";
+                }
+                progressBar1.Value += 1;
+                if (progressBar1.Value == 99)
                 {
                     timer1.Stop();
                 }
 
             }
         }
-
         private void Form1_Load(object sender, EventArgs e)
         {
             txtbxBenzin.Text = dieselPrice.ToString() + " ₺";
