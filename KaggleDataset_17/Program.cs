e2eb955a3a1040f14832fbdac79d8bdf81723ff1
Elif Yıldırım <elifyildirim3@outlook.com>
2025-04-27 23:55:21 +0300

    Proje 17 de kaggle sitesinden bir dataset nasıl localindeki database import edilir, import ederken alınan hatalar neler ve nasıl yol izlersin onu öğrendik. 3 farklı dataset verilerini mssql import etmeyi öğrendik. (task>import flat file)

---
+++ KaggleDataset_17/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaggleDataset_17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
