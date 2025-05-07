41e3f2ae5dff162cf0c808d8e00495c438013bd0
Elif Yıldırım <elifyildirim3@outlook.com>
2025-05-08 00:56:16 +0300

    App 19 finish

--- TableStatusMomentary_19/Form1.Designer.cs
+++ TableStatusMomentary_19/Form1.Designer.cs
@@ -28,20 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 363);
+            this.ClientSize = new System.Drawing.Size(642, 482);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- TableStatusMomentary_19/Form1.cs
+++ TableStatusMomentary_19/Form1.cs
@@ -16,5 +16,43 @@ namespace TableStatusMomentary_19
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            TestEntities context = new TestEntities();
+            var buttons = this.Controls.OfType<Button>().ToList();
+            foreach(var btn in buttons)
+            {
+                this.Controls.Remove(btn);
+            }
+
+            var values = context.TablesStatus.ToList();
+
+            int buttonWidth = 100;
+            int buttonHeight = 100;
+            int padding = 10;
+            int xOffset = 10;
+            int yOffset = 10;
+
+            for (int i = 0; i < values.Count; i++)
+            {
+                var item = values[i];
+                Button button = new Button();
+                //button.Text = $"Masa{i+1}";
+                button.Text = item.TableNumber?.ToString() ?? "---";
+                button.Size = new Size(buttonWidth, buttonHeight);
+                button.Location = new Point(xOffset + (i % 4) * (buttonWidth + padding),
+                    yOffset + (i / 4) * (buttonHeight + padding));
+
+                button.BackColor = item.Status.Value ? System.Drawing.Color.Green : System.Drawing.Color.Red; //eğer masa boşsa true ise yeşil, dolu false ise kırmızı olsun.
+
+                this.Controls.Add(button);
+            }
+        }
     }
 }
